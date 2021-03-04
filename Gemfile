264814947c760f965d6bfb93b0aa5579cc981f1e
Besmir Gjata <besigjata@gmail.com>
2021-03-04 14:46:58 +0100

    seeds done

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'font-awesome-sass', '~> 5.12.0'
 gem 'simple_form'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-
+gem 'pry', '~> 0.13.1'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+require 'pry'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+link = open(url).read
+cocktails = JSON.parse(link)
+cocktails["drinks"].each do |cocktail|
+  Ingredient.create(name: cocktail["strIngredient1"])
+end
+
+
+
+
+# Ingredient.create(name: cocktails.drinks[0]["strIngredient1"])
